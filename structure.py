6badbc77e707b1be29b7aed16ca0632fc5a2e2fd
bappa <iftakharul.alam@live.com>
2015-07-16 00:37:10 +0600

    grid layout

--- structure.py
+++ structure.py
@@ -7,15 +7,15 @@ class HelloWorld(QDialog):
     def __init__(self):
         QDialog.__init__(self)
 
-        layout = QHBoxLayout()
+        layout = QGridLayout()
 
         self.label = QLabel("Hello World!")
         line_edit = QLineEdit()
         button = QPushButton("Close")
 
-        layout.addWidget(self.label)
-        layout.addWidget(line_edit)
-        layout.addWidget(button)
+        layout.addWidget(self.label, 0, 0)
+        layout.addWidget(line_edit, 0, 1)
+        layout.addWidget(button, 1, 1)
 
         self.setLayout(layout)
         button.clicked.connect(self.close)
