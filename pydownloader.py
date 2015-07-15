189c9812966cbe3c5b1c505790b5995e270b37d3
bappa <iftakharul.alam@live.com>
2015-07-16 00:51:01 +0600

    new pydownloader app init

---
+++ pydownloader.py
@@ -0,0 +1,28 @@
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+import sys
+
+
+class Downloader(QDialog):
+    def __init__(self):
+        QDialog.__init__(self)
+
+        layout = QVBoxLayout()
+
+        url = QLineEdit()
+        save_location = QLineEdit()
+        progress = QProgressBar()
+        download = QPushButton("Download")
+
+        layout.addWidget(url)
+        layout.addWidget(save_location)
+        layout.addWidget(progress)
+        layout.addWidget(download)
+
+        self.setLayout(layout)
+
+
+app = QApplication(sys.argv)
+dl = Downloader()
+dl.show()
+app.exec_()
